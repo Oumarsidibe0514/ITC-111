9e30c4bd820d0a2a840e4853dad5ee735ccc12ed
Oumarsidibe0514 <63612473+Oumarsidibe0514@users.noreply.github.com>
2020-05-14 12:58:30 -0700

    this adds new javscript functions and a Sandbox

---
+++ Sandbox/switchStatementExample.js
@@ -0,0 +1,21 @@
+// Switch statement - gives multiple outcomes based on the value condition
+
+var seasons = 'spring';
+
+switch(seasons) {
+    case 'summer':
+        console.log('It\'s summer');
+        break;
+    case 'fall':
+        console.log('It\'s fall');
+        break;
+    case 'winter':
+        console.log('It\'s winter');
+        break;
+    case 'spring':
+        console.log('It\'s spring');
+        break;
+    default:
+            console.log('there are only 4 seasons broh!');
+            break;
+}
